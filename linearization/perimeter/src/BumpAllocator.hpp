549409fdcea2d79338950df7925e4101462b2edb
lsakka <laith.s.sakka@gmail.com>
2016-12-04 12:29:20 -0500

    add linearization code

---
+++ linearization/perimeter/src/BumpAllocator.hpp
@@ -0,0 +1,14 @@
+//
+//  BumpAllocator.hpp
+//  Arch_Project
+//
+//  Created by lsakka on 11/30/16.
+//  Copyright © 2016 lsakka. All rights reserved.
+//
+
+#ifndef BumpAllocator_hpp
+#define BumpAllocator_hpp
+
+#include <stdio.h>
+
+#endif /* BumpAllocator_hpp */
---
+++ linearization/treeadd/src/BumpAllocator.c
@@ -0,0 +1,32 @@
+//
+//  BumpAllocator.cpp
+//  Arch_Project
+//
+//  Created by lsakka on 11/30/16.
+//  Copyright © 2016 lsakka. All rights reserved.
+//
+
+#include "BumpAllocator.h"
+#include "stdlib.h"
+
+#define LINEARIZE
+char * heap_ptr ;
+void deAlloc(long long n){
+    heap_ptr -= n;
+}
+
+void initBumpAlloc(long long n){
+#ifdef LINEARIZE
+    heap_ptr=(char* ) malloc(n);
+#endif
+}
+char * alloc(long long n){
+#ifdef LINEARIZE
+    char * ret=heap_ptr;
+    heap_ptr += n;
+    return ret;
+#else
+    ret=malloc(n);
+#endif
+
+}
