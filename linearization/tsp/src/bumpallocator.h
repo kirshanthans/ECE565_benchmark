e8b043750b9b81b60410ee24fc39e8f5f2a2286a
lsakka <laith.s.sakka@gmail.com>
2016-12-06 22:26:46 -0500

    add tsp

---
+++ linearization/tsp/src/bumpallocator.h
@@ -0,0 +1,24 @@
+//
+//  BumpAllocator.hpp
+//  Arch_Project
+//
+//  Created by lsakka on 11/30/16.
+//  Copyright © 2016 lsakka. All rights reserved.
+//
+
+#ifndef bumpallocator_h
+#define bumpallocator_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char * heap_ptr ;
+
+char * alloc(long long  n);
+void deAlloc(long long n);
+void initBumpAlloc(long long n );
+
+
+
+
+#endif /* BumpAllocator_hpp */
