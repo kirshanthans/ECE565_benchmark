4e3825de18a9d8e78752139f273ae1c39154ba67
lsakka <laith.s.sakka@gmail.com>
2016-12-07 20:44:12 -0500

    fix

--- linearization/treeadd/src/node.c
+++ linearization/treeadd/src/node.c
@@ -2,6 +2,9 @@
 
 /* node.c
  */
+#define TORONTO
+#include "BumpAllocator.h"
+#include <math.h>
 #ifndef TORONTO
 #include <cm/cmmd.h>
 #endif
@@ -31,7 +34,7 @@ int main (int argc, char *argv[])
 {
     tree_t	*root;
     int level,result;
-
+    
 #ifdef FUTURES
     level = SPMDInit(argc,argv);
 #else
@@ -46,27 +49,27 @@ int main (int argc, char *argv[])
 #endif
 #ifdef TORONTO
     chatting("Treeadd with %d levels on %d processors \n",
-	     level, NumNodes);
+             level, NumNodes);
 #else
     chatting("Treeadd with %d levels on %d processors \n",
-	     level, __NumNodes);
+             level, __NumNodes);
 #endif
     /* only processor 0 will continue here. */
-    chatting("About to enter TreeAlloc\n"); 
+    chatting("About to enter TreeAlloc\n");
 #ifndef TORONTO
     CMMD_node_timer_start(0);
 #endif
-
+    initBumpAlloc(pow(2, level+1)*sizeof(tree_t));
 #ifdef TORONTO
     root = TreeAlloc (level, 0, NumNodes);
 #else
     root = TreeAlloc (level, 0, __NumNodes);
 #endif
-
+    
 #ifndef TORONTO
     CMMD_node_timer_stop(0);
 #endif
-    chatting("About to enter TreeAdd\n"); 
+    chatting("About to enter TreeAdd\n");
     
 #ifndef PLAIN
     ClearAllStats();
@@ -74,70 +77,70 @@ int main (int argc, char *argv[])
 #ifndef TORONTO
     CMMD_node_timer_start(1);
 #endif
-{ int i; for (i = 0; i < 100; ++i)
-    result = TreeAdd (root);
-}
+    { int i; for (i = 0; i < 100; ++i)
+        result = TreeAdd (root);
+    }
 #ifndef TORONTO
     CMMD_node_timer_stop(1);
 #endif
     chatting("Received result of %d\n",result);
-
+    
 #ifndef TORONTO
     chatting("Alloc Time = %f seconds\n", CMMD_node_timer_elapsed(0));
     chatting("Add Time = %f seconds\n", CMMD_node_timer_elapsed(1));
 #endif
-
+    
 #ifdef FUTURES
     __ShutDown();
 #endif
     exit(0);
-
-
+    
+    
 }
 
 /* TreeAdd:
  */
 int TreeAdd (tree_t *t)
 {
-  if (t == NULL)  {
-    return 0;
-  }
-  else {
-#ifdef FUTURES    
-    future_cell_int leftval;
-    int rightval;
-    tree_t *tleft, *tright;
-    int value;
-
-    tleft = t->left;
-    RPC(tleft, tleft,TreeAdd,&(leftval));
-    NOTEST();
-    tright = t->right;
-    rightval = TreeAdd(tright);
-    RTOUCH(&leftval);
-    /*chatting("after touch @ 0x%x\n",t);*/
-    value = t->val;
-    /*chatting("returning from treeadd %d\n",*/
-	     /*leftval.value + rightval.value + value);*/
-    return leftval.value + rightval + value;
-    RETEST();
+    if (t == NULL)  {
+        return 0;
+    }
+    else {
+#ifdef FUTURES
+        future_cell_int leftval;
+        int rightval;
+        tree_t *tleft, *tright;
+        int value;
+        
+        tleft = t->left;
+        RPC(tleft, tleft,TreeAdd,&(leftval));
+        NOTEST();
+        tright = t->right;
+        rightval = TreeAdd(tright);
+        RTOUCH(&leftval);
+        /*chatting("after touch @ 0x%x\n",t);*/
+        value = t->val;
+        /*chatting("returning from treeadd %d\n",*/
+        /*leftval.value + rightval.value + value);*/
+        return leftval.value + rightval + value;
+        RETEST();
 #else
-    int leftval;
-    int rightval;
-    tree_t *tleft, *tright;
-    int value;
-
-    tleft = t->left;            /* <---- 57% load penalty */
-    leftval = TreeAdd(tleft);
-    tright = t->right;          /* <---- 11.4% load penalty */
-    rightval = TreeAdd(tright);
-    /*chatting("after touch\n");*/
-    value = t->val;             
-    /*chatting("returning from treeadd %d\n",*/
-	     /*leftval.value + rightval.value + value);*/
-    return leftval + rightval + value;
+        int leftval;
+        int rightval;
+        tree_t *tleft, *tright;
+        int value;
+        
+        tleft = t->left;            /* <---- 57% load penalty */
+        leftval = TreeAdd(tleft);
+        tright = t->right;          /* <---- 11.4% load penalty */
+        rightval = TreeAdd(tright);
+        /*chatting("after touch\n");*/
+        value = t->val;
+        /*chatting("returning from treeadd %d\n",*/
+        /*leftval.value + rightval.value + value);*/
+        return leftval + rightval + value;
 #endif
-  }
+    }
 } /* end of TreeAdd */
 
 
--- linearization/treeadd/src/par-alloc.c
+++ linearization/treeadd/src/par-alloc.c
@@ -2,26 +2,27 @@
 
 /* tree-alloc.c
  */
-
+#define TORONTO
 #ifndef TORONTO
 #include "mem-ref.h"
 #include "future-cell.h"
 #endif
 
+#include "BumpAllocator.h"
 #include "tree.h"
-extern void *malloc(unsigned);
+//extern void *malloc(unsigned);
 
 tree_t *TreeAlloc (int level, int lo, int proc) {
-  if (level == 0)
-    return NULL;
-  else {
-    struct tree *new, *right, *left;
-    new = (struct tree *) malloc(sizeof(tree_t));
-    left = TreeAlloc(level -1, lo+proc/2, proc/2);
-    right=TreeAlloc(level-1,lo,proc/2);
-    new->val = 1;
-    new->left = (struct tree *) left;
-    new->right = (struct tree *) right;
-    return new;
-  }
+    if (level == 0)
+        return NULL;
+    else {
+        struct tree *new, *right, *left;
+        new = (struct tree *) alloc(sizeof(tree_t));
+        left = TreeAlloc(level -1, lo+proc/2, proc/2);
+        right=TreeAlloc(level-1,lo,proc/2);
+        new->val = 1;
+        new->left = (struct tree *) left;
+        new->right = (struct tree *) right;
+        return new;
+    }
 }
